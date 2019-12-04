e711fd8ea9b6580696ef98a5b7537b3445d64cc9
DK <davidkkimball@gmail.com>
2019-12-04 00:11:42 -0500

    Updated torches in menu

---
+++ Assets/Scripts/waterfallTransparencyControl.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class waterfallTransparencyControl : MonoBehaviour
+{
+    public Animator anim;
+    public GameObject waterfall;
+    public bool reload;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject.tag == "Player" && reload == true)
+        {
+        Debug.Log("hit!");
+        //anim.Play("waterfallTransparency"); 
+        waterfall.SetActive(false);
+        reload = false;
+        }       
+    }
+
+private void OnTriggerExit(Collider other)
+{
+        if(other.gameObject.tag == "Player" && reload == true)
+        {
+        //anim.Play("waterfallReturnOpaque"); 
+        waterfall.SetActive(true);
+        reload = true;
+        }         
+}
+
+}
