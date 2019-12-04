cd0421455ce73972d344b5bc6bbaeca9696c4a7f
Twonny98 <42622799+Twonny98@users.noreply.github.com>
2019-12-04 01:05:09 -0500

    commiting

--- Assets/Scripts/MenuScripts/GammaSlider.cs
+++ Assets/Scripts/MenuScripts/GammaSlider.cs
@@ -6,9 +6,9 @@ public class GammaSlider : MonoBehaviour
 {
     public float rgbValue = 0.5f;
 
-    public void ChangeLight()
+    public void ChangeLight(float newGamma)
     {
-        rgbValue = GUI.HorizontalSlider(new Rect(Screen.width / 2 - 50, 90, 100, 30), rgbValue, 0f, 1.0f);
+        rgbValue = newGamma;
         RenderSettings.ambientLight = new Color(rgbValue, rgbValue, rgbValue, 1);
     }
 
