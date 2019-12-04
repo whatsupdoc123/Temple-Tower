f4b8c3ad6a89dd8fa60205caa763b588cb29110c
Twonny98 <42622799+Twonny98@users.noreply.github.com>
2019-12-04 00:25:51 -0500

    commiting cuz it told me to

---
+++ Assets/Scripts/MenuScripts/GammaSlider.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GammaSlider : MonoBehaviour
+{
+    public float rgbValue = 0.5f;
+
+    public void ChangeLight()
+    {
+        rgbValue = GUI.HorizontalSlider(new Rect(Screen.width / 2 - 50, 90, 100, 30), rgbValue, 0f, 1.0f);
+        RenderSettings.ambientLight = new Color(rgbValue, rgbValue, rgbValue, 1);
+    }
+
+}
---
+++ Assets/Scripts/MenuScripts/VolumeSlider.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class VolumeSlider : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+
+    public void SetVolume (float volume)
+    {
+        audioMixer.SetFloat("MasterVolume", volume);
+    }
+}
