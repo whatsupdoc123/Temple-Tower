aa90e5ace86439ad5bc97a1b2e3b59d3feb850a7
Twonny98 <apinerocrespo@knights.ucf.edu>
2019-12-04 23:31:07 -0500

    Waterfall

---
+++ Assets/waterfallForce.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class waterfallForce : MonoBehaviour
+{
+    public bool isForced = false;
+    public Collision player;
+
+    private void Update()
+    {
+        if (isForced)
+        {
+            player.rigidbody.AddForce(-transform.right * .75f, ForceMode.VelocityChange);
+        }
+    }
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            isForced = true;
+            player = collision;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            isForced = false;
+            player.rigidbody.velocity = new Vector3(0f, player.rigidbody.velocity.y, 0f);
+        }
+    }
+}
