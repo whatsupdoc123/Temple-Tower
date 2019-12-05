49e808ec93f1f02d1905caf2c5fcd4fab2fb814a
Twonny98 <apinerocrespo@knights.ucf.edu>
2019-12-04 19:30:18 -0500

    Menuprettier

--- Assets/SettingsMenu.cs
+++ Assets/SettingsMenu.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SettingsMenu : MonoBehaviour
 {
@@ -9,10 +10,11 @@ public class SettingsMenu : MonoBehaviour
     public GameObject resumeButton;
     public GameObject quitButton;
     public GameObject backButton;
-    public GameObject audioText;
+    public GameObject gammaSlider;
     public GameObject slider1;
     public GameObject slider2;
     public GameObject slider3;
+    public GameObject gammaText;
     public GameObject masterText;
     public GameObject musicText;
     public GameObject sfxText;
@@ -25,10 +27,11 @@ public class SettingsMenu : MonoBehaviour
         resumeButton.SetActive(false);
         quitButton.SetActive(false);
         backButton.SetActive(true);
-        audioText.SetActive(true);
+        gammaSlider.SetActive(true);
         slider1.SetActive(true);
         slider2.SetActive(true);
         slider3.SetActive(true);
+        gammaText.SetActive(true);
         musicText.SetActive(true);
         masterText.SetActive(true);
         sfxText.SetActive(true);
@@ -40,12 +43,18 @@ public class SettingsMenu : MonoBehaviour
         resumeButton.SetActive(true);
         quitButton.SetActive(true);
         backButton.SetActive(false);
-        audioText.SetActive(false);
+        gammaSlider.SetActive(false);
         slider1.SetActive(false);
         slider2.SetActive(false);
         slider3.SetActive(false);
+        gammaText.SetActive(false);
         musicText.SetActive(false);
         masterText.SetActive(false);
         sfxText.SetActive(false);
+    }
+
+    public void Quit()
+    {
+        SceneManager.LoadScene("Menu");
     }
 }
