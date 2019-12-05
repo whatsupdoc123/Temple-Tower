980c9a6990ae32a271c476420c1cc3a4baa1c25a
Twonny98 <apinerocrespo@knights.ucf.edu>
2019-12-04 20:15:01 -0500

    MenuFunctionalExceptSFXSlider

---
+++ Assets/SFXChanger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SFXChanger : MonoBehaviour
+{
+    public AudioSource sfx1;
+    private float sfxVolume;
+
+    void Start()
+    {
+        sfx1 = GetComponent<AudioSource>();
+    }
+
+
+    void Update()
+    {
+        sfx1.volume = sfxVolume;
+    }
+
+    public void SetVolume(float vol)
+    {
+        sfxVolume = vol;
+    }
+}
